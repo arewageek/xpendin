a93782bed8832b3fe399d1ba50ebfe8bff1262dc
arewageek <arewageek@gmail.com>
2023-11-01 21:30:53 +0100

    Average Gas calculations setup

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -6,17 +6,33 @@ import { Web3Context } from '../context/Web3Context'
 
 export const Transactions = () => {
     const [transactions, setTransactions] = useState([])
+    const [averageGas, setAverageGas] = useState(0)
 
-    const { web3 } = useContext(Web3Context)
+    const { web3, gasFormatter } = useContext(Web3Context)
 
     useEffect(() => {
         web3 && fetchTrx()
     }, [web3])
 
     useEffect(() => {
-        transactions && console.log(transactions)
+        return async () => {
+            if (transactions.length > 0) {
+                console.log(transactions)
+                const totalGas = transactions.reduce(
+                    (total, tx) => total + parseInt(tx.gas),
+                    0
+                );
+                const avgGas = totalGas / transactions.length;
+                const formattedGasAvg = await gasFormatter(avgGas)
+                setAverageGas(formattedGasAvg);
+            }
+        }
     }, [transactions])
 
+    useEffect(() => {
+        // console.log("Average Gas Price: " + averageGas)
+    })
+    
     const fetchTrx = async () => {
         try{
             // console.log(web3)
@@ -31,8 +47,10 @@ export const Transactions = () => {
 
 
                     allData.unshift(trxByHash)
+
+                    const last50trx = allData.slice(0,50)
                 
-                    setTransactions(allData.slice(0,50));
+                    setTransactions(last50trx);
                 })
             }
         }
@@ -44,6 +62,13 @@ export const Transactions = () => {
     return (
         <Container>
             <div className='w-full bg-gray-50 dark:bg-gray-700 min-h-[200pt] shadow-xl p-4 rounded-2xl overflow-x-auto'>
+
+                <div className="w-full flex justify-end">
+                    <div className='shadow px-3 py-2 text-xs font-bold rounded-lg my-4 mx-2 text-sky-800'>
+                        Average Gas: { averageGas } Gwei
+                    </div>
+                </div>
+                
                 <table className='table table-auto w-full text-xs text-left border-2 border-gray-200 dark:border-gray-800 border-collapse text-gray-600 dark:text-gray-300'>
                     <thead className='border-2 border-gray-200 dark:border-gray-600'>
                         <tr className='border-2 border-gray-200 dark:border-gray-600'>
@@ -61,7 +86,9 @@ export const Transactions = () => {
                     <tbody>
                         
                         { 
+                        
                             transactions.length > 0 ? (
+                                
                                 transactions.map((tx, index) => (
                                     <Row data={tx} index={index + 1} key={index + 1} />
                                 ))
