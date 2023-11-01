f32db2c2c24db856b4b3d9521b1fff5d336e2b27
arewageek <arewageek@gmail.com>
2023-11-01 19:50:47 +0100

    Hyperlinked Transaction Hash, From and To Addresses

--- src/components/Row.js
+++ src/components/Row.js
@@ -30,17 +30,17 @@ export const Row = ({ index, data }) => {
                 {index}
             </td>
             <td className='p-2 border-2 border-gray-200 dark:border-gray-600 font-semibold italic'>
-                <a href={`https://sepolia.etherscan.io/tx/${data.hash}`} className='underline text-sky-800 dark:text-sky-300'>
+                <a target="_blank" href={`https://sepolia.etherscan.io/tx/${data.hash}`} className='underline text-sky-800 dark:text-sky-300'>
                     { data.hash && data.hash.slice(0,6) }...{ data.hash && data.hash.slice(-7) }
                 </a>
             </td>
             <td className='p-2 border-2 border-gray-200 dark:border-gray-600 font-semibold italic'>
-                <a href={`https://sepolia.etherscan.io/address/${data.from}`} className='underline text-sky-800 dark:text-sky-300'>
+                <a target="_blank" href={`https://sepolia.etherscan.io/address/${data.from}`} className='underline text-sky-800 dark:text-sky-300'>
                     { data.from && data.from.slice(0,6) }...{ data.from && data.from.slice(-7) }
                 </a>
             </td>
             <td className='p-2 border-2 border-gray-200 dark:border-gray-600 font-semibold italic'>
-                <a href={`https://sepolia.etherscan.io/address/${data.to}`} className='underline text-sky-800 dark:text-sky-300'>
+                <a target="_blank" href={`https://sepolia.etherscan.io/address/${data.to}`} className='underline text-sky-800 dark:text-sky-300'>
                     { data.to && data.to.slice(0,6) }...{ data.to && data.to.slice(-7) }
                 </a>
             </td>
