d0122ec119cb9df47aab65d72c319d31caab05b0
arewageek <arewageek@gmail.com>
2023-11-01 19:49:41 +0100

    Hyperlinked Transaction Hash, From and To Addresses

--- src/components/Row.js
+++ src/components/Row.js
@@ -30,13 +30,19 @@ export const Row = ({ index, data }) => {
                 {index}
             </td>
             <td className='p-2 border-2 border-gray-200 dark:border-gray-600 font-semibold italic'>
-                { data.hash && data.hash.slice(0,6) }...{ data.hash && data.hash.slice(-7) }
+                <a href={`https://sepolia.etherscan.io/tx/${data.hash}`} className='underline text-sky-800 dark:text-sky-300'>
+                    { data.hash && data.hash.slice(0,6) }...{ data.hash && data.hash.slice(-7) }
+                </a>
             </td>
             <td className='p-2 border-2 border-gray-200 dark:border-gray-600 font-semibold italic'>
-                { data.from && data.from.slice(0,6) }...{ data.from && data.from.slice(-7) }
+                <a href={`https://sepolia.etherscan.io/address/${data.from}`} className='underline text-sky-800 dark:text-sky-300'>
+                    { data.from && data.from.slice(0,6) }...{ data.from && data.from.slice(-7) }
+                </a>
             </td>
             <td className='p-2 border-2 border-gray-200 dark:border-gray-600 font-semibold italic'>
-                { data.to && data.to.slice(0,6) }...{ data.to && data.to.slice(-7) }
+                <a href={`https://sepolia.etherscan.io/address/${data.to}`} className='underline text-sky-800 dark:text-sky-300'>
+                    { data.to && data.to.slice(0,6) }...{ data.to && data.to.slice(-7) }
+                </a>
             </td>
             <td className='p-2 border-2 border-gray-200 dark:border-gray-600 font-semibold italic'>
                 { data.data && data.data.slice(0,7) }{data.data.length > 7 ? '...' : ''}
@@ -47,10 +53,10 @@ export const Row = ({ index, data }) => {
                 } Eth
             </td>
             <td className='p-2 border-2 border-gray-200 dark:border-gray-600 font-semibold italic'>
-                { gas && Number(gas).toFixed(5) } Gwei
+                { gas && Number(gas).toFixed(4) } Gwei
             </td>
             <td className='p-2 border-2 border-gray-200 dark:border-gray-600 font-semibold italic'>
-                { gasPrice && Number(gasPrice).toFixed(5) } Gwei
+                { gasPrice && Number(gasPrice).toFixed(4) } Gwei
             </td>
         </tr>
         
